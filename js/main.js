f5fe933cf69f3187a02fd85c612ceb8f4ed2fe4c
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-01-27 12:58:29 +0200

    add btn scroll top

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
+
 const navbar = document.querySelector('nav.navbar');
+const navItems = document.querySelectorAll('.nav-item');
+const btnScrollTop = document.querySelector('#ScrollTop');
+/**
+ * remove active class from all nav items
+ * add active class to the clicked nav item
+ */
+for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', function() {
+        for (let j = 0; j < navItems.length; j++) {
+            navItems[j].classList.remove('active');
+        }
+        navItems[i].classList.add('active');
+    });
+}
 
 
 
+
+
+/**
+ * add padding top and bottom from navbar when scrolled
+ * remove padding top and bottom to navbar when not scrolled
+ */
 document.addEventListener("scroll", function() {  
     
     const scroll = window.scrollY;
@@ -21,4 +42,25 @@ document.addEventListener("scroll", function() {
         navbar.classList.add('pt-3');
         navbar.classList.remove('pt-4');
     }
+
+    if (scroll > 100) {
+        btnScrollTop.classList.remove('opacity-0');
+        btnScrollTop.classList.add('opacity-100');
+    }else{
+        btnScrollTop.classList.add('opacity-0');
+        btnScrollTop.classList.remove('opacity-100');
+    }
+
+
 })
+
+
+btnScrollTop.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Smooth scrolling
+    });
+
+
+
+  });
