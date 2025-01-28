2d3e3c4983c7327a4d4d2d3e32948e70e27bcdb5
Menna-Ashraf512 <mennaalla@gmail.com>
2025-01-28 16:27:02 +0200

    menna

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 
-import Splide from '@splidejs/splide';
+// import Splide from '@splidejs/splide';
 
 
-new Splide( '.splide' ).mount();
+// new Splide( '.splide' ).mount();
+
+
+// start carousel-two section 
+const sliderm = new Sliderm('#exampe-slider', {
+    arrow: false,
+    pagination: true,
+    grouping: false,
+    loop: false,
+    preview: false,
+    columns: 2,
+    duration: 1000,
+    spacing: 0,
+    align: 'center',
+  });
+
+  
+
+  // End carousel-two section 
+
+  
+  const customCursor = document.querySelector('.custom-cursor');
+
+  document.addEventListener('mousemove', (e) => {
+    customCursor.style.top = `${e.clientY}px`;
+    customCursor.style.left = `${e.clientX}px`;
+  });
